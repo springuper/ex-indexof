55a6710e97074d79e5a18c5c273438b79c3aaf02
shangchun <shangchun@meituan.com>
2015-09-16 10:09:15 +0800

    refactor

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 module.exports = function (str, substr, time, overlap) {
+    var step = overlap ? 1 : substr.length;
     if (time < 0) {
         time = -1 - time;
-        var step = overlap ? 1 : substr.length;
         var pos = str.length - 1 + step;
-
         do {
             if (pos < step) return -1;
             pos = str.lastIndexOf(substr, pos - step);
             if (pos === -1) return -1;
         } while (time--)
     } else {
-        var step = overlap ? 1 : substr.length;
         var pos = 0 - step;
-
         do {
             pos = str.indexOf(substr, pos + step);
             if (pos === -1) return -1;
